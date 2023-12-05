471ed07548cb0b7b8b001228ed5036f87e0696bf
Tegarr123 <fadhino987@gmail.com>
2023-12-05 09:42:03 +0700

    update 9.41

--- src/sudoku/GameBoardPanel.java
+++ src/sudoku/GameBoardPanel.java
@@ -2,8 +2,47 @@ package sudoku;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GameBoardPanel extends JPanel {
+    private class CellInputListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // Get a reference of the JTextField that triggers this action event
+            Cell sourceCell = (Cell)e.getSource();
+
+            // Retrieve the int entered
+            int numberIn = Integer.parseInt(sourceCell.getText());
+            // For debugging
+            System.out.println("You entered " + numberIn);
+
+            /*
+             * [TODO 5] (later - after TODO 3 and 4)
+             * Check the numberIn against sourceCell.number.
+             * Update the cell status sourceCell.status,
+             * and re-paint the cell via sourceCell.paint().
+             */
+            //if (numberIn == sourceCell.number) {
+            //   sourceCell.status = CellStatus.CORRECT_GUESS;
+            //} else {
+            //   ......
+            //}
+            //sourceCell.paint();   // re-paint this cell based on its status
+            if(numberIn == sourceCell.number){
+                sourceCell.status = CellStatus.CORRECT_GUESS;
+            }else{
+                sourceCell.status = CellStatus.WRONG_GUESS;
+            }
+            sourceCell.paint();
+
+            /*
+             * [TODO 6] (later)
+             * Check if the player has solved the puzzle after this move,
+             *   by calling isSolved(). Put up a congratulation JOptionPane, if so.
+             */
+        }
+    }
     private static final long serialVersionUID = 1L;
 
     public static final int CELL_SIZE = 60;
@@ -14,16 +53,20 @@ public class GameBoardPanel extends JPanel {
     private Puzzle puzzle = new Puzzle();
 
     public GameBoardPanel(){
+        CellInputListener listener = new CellInputListener();
         super.setLayout(new GridLayout(SudokuConstants.GRID_SIZE, SudokuConstants.GRID_SIZE));
         for (int row = 0; row < SudokuConstants.GRID_SIZE; row++){
             for (int col = 0 ; col < SudokuConstants.GRID_SIZE;col++){
                 cells[row][col] = new Cell(row,col);
                 super.add(cells[row][col]);
+                if (cells[row][col].isEditable()) {
+                    cells[row][col].addActionListener(listener);   // For all editable rows and cols
+                }
             }
         }
 
 
-        super.setPreferredSize(new Dimension(BOARD_HEIGHT, BOARD_WIDTH));
+        super.setPreferredSize(new Dimension(BOARD_WIDTH, BOARD_HEIGHT));
     }
     public void newGame(){
         puzzle.newPuzzle(2);
--- src/sudoku/Puzzle.java
+++ src/sudoku/Puzzle.java
@@ -19,6 +19,14 @@ public class Puzzle {
                  {9, 6, 1, 5, 3, 7, 2, 8, 4},
                  {2, 8, 7, 4, 1, 9, 6, 3, 5},
                  {3, 4, 5, 2, 8, 6, 1, 7, 9}};
+        // Random generator
+        int random = (int) (Math.random() * 8);
+        int random2 = (int) (Math.random() * 8);
+
+        for(int i = 0 ; i < random2 ; i++){
+           int subGrid = i/3;
+        }
+
         for (int row = 0 ; row < SudokuConstants.GRID_SIZE ; row++){
             for (int col = 0 ; col < SudokuConstants.GRID_SIZE ; col++){
                 numbers[row][col] = hardcodedNumbers[row][col];
