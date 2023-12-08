b2ff5a52c87b26ec1f3994f85fccb434988f83e8
Tegarr123 <fadhino987@gmail.com>
2023-12-08 23:12:25 +0700

    header

--- src/sudoku/view/GameBoardPanel.java
+++ src/sudoku/view/GameBoardPanel.java
@@ -1,3 +1,12 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2023/2024
+ * Group Capstone Project
+ * Group #7
+ * 1 - 5026221169 - R. Farel Danendra Hendrawan Putra
+ * 2 - 5026221095 - Nida Aulia Amartika
+ * 3 - 5026221109 - Ahmad Fadhino Tegar Permana
+ */
 package sudoku.view;
 
 import sudoku.controller.SudokuMain;
